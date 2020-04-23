c81fe5d67b43d8854ae339666401fe569f53268f
Stephanie Cheney <steph@scheneyphotography.com>
2020-04-22 22:27:44 -0500

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,23 @@
+class Post
+attr_accessor :author, :title
+
+@@all=[]
+
+def initialize(title)
+    @title=title
+    @@all << self 
+end 
+
+def self.all
+    @@all
+end 
+
+def author_name
+    if !self.author
+        puts nil
+    else 
+    self.author.name
+    end 
+end 
+
+end
